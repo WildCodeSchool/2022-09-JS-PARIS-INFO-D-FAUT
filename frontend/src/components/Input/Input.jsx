4872c4ed47571c4213e1c053f70d37ce65ca2f99
AndreaCastel <marion.chateau@sncf.fr>
2022-09-05 16:46:23 +0200

    add Input.jsx and Input.css components

---
+++ frontend/src/components/Input/Input.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import "./Input.css";
+
+export const Input = ({
+    type,
+    champ,
+    forId,
+    onChange,
+    defaultValue,
+    value,
+    onClick,
+    placeholder,
+    minlength,
+    maxlength,
+  }) => {
+    return (
+      <div className="input-container">
+        <label className="label-text" htmlFor={forId}>
+          {champ}
+        </label>
+        <input
+          className="input-text"
+          type={type}
+          id={forId}
+          name={champ}
+          onChange={onChange}
+          defaultValue={defaultValue}
+          value={value}
+          onClick={onClick}
+          placeholder={placeholder}
+          minlength={minlength}
+          maxlength={maxlength}
+        />
+      </div>
+    );
+  };
