429af3390b541f8332a81e9f6c89f9bedd4a4b17
Marina Alves <ad.marina@hotmail.fr>
2022-09-06 16:23:32 +0200

    add textarea and CSS + on each pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,37 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/Home/Home";
-// import GareEtConnexions from "./pages/GareEtConnexions/GareEtConnexions";
-// import Ter from "./pages/Ter/Ter";
-// import Reseau from "./pages/Reseau/Reseau";
-// import Voyageurs from "./pages/Voyageurs/Voyageurs";
-// import Profile from "./pages/Profile/Profile";
-// import { LatitudeContext } from "./context/LatitudeContext";
-// import { LongitudeContext } from "./context/LongitudeContext";
-// import { Utilisateur } from "./pages/Utilisateur/Utilisateur";
+import GareEtConnexions from "./pages/GareEtConnexions/GareEtConnexions";
+import Ter from "./pages/Ter/Ter";
+import Reseau from "./pages/Reseau/Reseau";
+import Voyageurs from "./pages/Voyageurs/Voyageurs";
+import Profile from "./pages/Profile/Profile";
+import { LatitudeContext } from "./context/LatitudeContext";
+import { LongitudeContext } from "./context/LongitudeContext";
+import { Utilisateur } from "./pages/Utilisateur/Utilisateur";
 
 function App() {
-  // const [latitude, setLatitude] = useState("");
-  // const [longitude, setLongitude] = useState("");
+  const [latitude, setLatitude] = useState("");
+  const [longitude, setLongitude] = useState("");
   return (
-    // <LatitudeContext.Provider value={{ latitude, setLatitude }}>
-    // <LongitudeContext.Provider value={{ longitude, setLongitude }}>
-    <Router>
-      <div className="App">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          {/* <Route path="/gare-et-connexions" element={<GareEtConnexions />} /> */}
-          {/* <Route path="/ter" element={<Ter />} /> */}
-          {/* <Route path="/reseau" element={<Reseau />} /> */}
-          {/* <Route path="/voyageurs" element={<Voyageurs />} /> */}
-          {/* <Route path="/profile" element={<Profile />} /> */}
-          {/* <Route path="/utilisateur" element={<Utilisateur />} /> */}
-        </Routes>
-      </div>
-    </Router>
-    // </LongitudeContext.Provider>
-    // </LatitudeContext.Provider>
+    <LatitudeContext.Provider value={{ latitude, setLatitude }}>
+      <LongitudeContext.Provider value={{ longitude, setLongitude }}>
+        <Router>
+          <div className="App">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route
+                path="/gare-et-connexions"
+                element={<GareEtConnexions />}
+              />
+              <Route path="/ter" element={<Ter />} />
+              <Route path="/reseau" element={<Reseau />} />
+              <Route path="/voyageurs" element={<Voyageurs />} />
+              <Route path="/profile" element={<Profile />} />
+              <Route path="/utilisateur" element={<Utilisateur />} />
+            </Routes>
+          </div>
+        </Router>
+      </LongitudeContext.Provider>
+    </LatitudeContext.Provider>
   );
 }
 
--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -3,3 +3,4 @@ export { Footer } from "./Footer/Footer";
 export { Button } from "./Button/Button";
 export { Input } from "./Input/Input";
 export { Item } from "./Item/Item";
+export { Textarea } from "./Textarea/Textarea";
