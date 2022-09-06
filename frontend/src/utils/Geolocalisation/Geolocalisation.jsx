e50f277b6a7560e9370bfc6da2c8ed56d2e40440
Rico-Developpeur <eric.vieira@sncf.fr>
2022-09-06 11:41:49 +0200

    creation of the geolocation function, creation of the station and connections page

---
+++ frontend/src/utils/Geolocalisation/Geolocalisation.jsx
@@ -0,0 +1,16 @@
+import { useEffect, useContext } from "react";
+import { LatitudeContext } from "../../context/LatitudeContext";
+import { LongitudeContext } from "../../context/LongitudeContext";
+import "./Geolocalisation.css";
+
+export function Geolocalisation() {
+  const { setLatitude } = useContext(LatitudeContext);
+  const { setLongitude } = useContext(LongitudeContext);
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      setLatitude(position.coords.latitude);
+      setLongitude(position.coords.longitude);
+    });
+  }, [setLatitude, setLongitude]);
+}
