97284b0a07b27abf1e166908265508c63a40a7a3
antoine carpentier <a.carpentier.wcs@gmail.com>
2022-09-06 11:55:46 +0200

    creation Utilisateur, Utilisateur.jsx and Utilisateur.css

---
+++ frontend/src/pages/Utilisateur/Utilisateur.jsx
@@ -0,0 +1,61 @@
+import { Header, Footer, Input, Button } from "../../components/index";
+import React, { useRef } from "react";
+import { Link } from "react-router-dom";
+import "./Utilisateur.css";
+
+export const Utilisateur = () => {
+  const formRef = useRef(null);
+
+  const onSubmit = (evt) => {
+    evt.preventDefault();
+
+    if (!formRef) {
+      return;
+    }
+
+    fetch(formRef.current.action, {
+      method: formRef.current.method,
+      body: new FormData(formRef.current),
+    });
+  };
+
+  return (
+    <div className="utilisateur-container">
+      <Header backCss="backProfile" profileCss="profileProfile" />
+      <div className="champ-container">
+        <form
+          className="form-container"
+          ref={formRef}
+          action="/upload-utilisateur"
+          method="POST"
+          onSubmit={onSubmit}
+        >
+          <h1>UTILISATEUR</h1>
+          <Input
+            forId="cp"
+            type="text"
+            champ="Numéro de CP"
+            // minlength="8"
+            // maxlength="8"
+          />
+          <Input forId="mot" type="password" champ="Mot de passe" />
+          <Button
+            classButton="envoyer"
+            // onClick={(e) => e}
+            champButton="CONNEXION"
+            type="submit"
+          />
+        </form>
+        <div className="trait"></div>
+        <Link className="link" to="/profile">
+          <Button
+            classButton="inscription"
+            champButton="S'INSCRIRE"
+            type="submit"
+          />
+        </Link>
+      </div>
+      <Footer />
+    </div>
+  );
+};
