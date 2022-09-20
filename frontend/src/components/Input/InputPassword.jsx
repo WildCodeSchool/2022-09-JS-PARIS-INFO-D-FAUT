2b42da59ddf25dbb9807fdbbfc3ddb8e09b79d78
antoine carpentier <a.carpentier.wcs@gmail.com>
2022-09-20 16:28:19 +0200

    add Show Password(add inputPassword.jsx and .css) and begin retouch css

---
+++ frontend/src/components/Input/InputPassword.jsx
@@ -0,0 +1,51 @@
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+import React, { useState } from "react";
+import "./InputPassword.css";
+import closereye from "../../assets/eye-closer.svg";
+import openeye from "../../assets/eye-open.svg";
+
+export const Password = ({
+  champ,
+  forId,
+  onChange,
+  defaultValue,
+  value,
+  onClick,
+  placeholder,
+  minlength,
+  maxlength,
+  className,
+}) => {
+  const [passwordVissible, setPasswordVisible] = useState(false);
+  const click = () => setPasswordVisible((prevState) => !prevState);
+  return (
+    <div className="inputPassword-container">
+      <label className="label-text" htmlFor={forId}>
+        {champ}
+      </label>
+      <label className="enterPassword">
+        <input
+          className="inputPassword"
+          type={passwordVissible ? "text" : "password"}
+          id={forId}
+          name={champ}
+          onChange={onChange}
+          defaultValue={defaultValue}
+          value={value}
+          onClick={onClick}
+          placeholder={placeholder}
+          minLength={minlength}
+          maxLength={maxlength}
+        />
+        <span onClick={click}>
+          <img
+            className="eye"
+            src={passwordVissible ? closereye : openeye}
+            alt=""
+          />
+        </span>
+      </label>
+    </div>
+  );
+};
