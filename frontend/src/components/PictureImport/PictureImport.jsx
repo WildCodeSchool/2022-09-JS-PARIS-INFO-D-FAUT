4bd5f2f8fde2633c70233175d4596a8553d3fa4a
AndreaCastel <marion.chateau@sncf.fr>
2022-09-21 12:10:08 +0200

    PictureImportCSS

--- frontend/src/components/PictureImport/PictureImport.jsx
+++ frontend/src/components/PictureImport/PictureImport.jsx
@@ -1,6 +1,6 @@
 import { React, useState, Alert } from "react";
 import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
-import { storage } from "./firebase";
+import { storage } from "../../services/Firebase/firebase";
 import "./PictureImport.css";
 
 export const PictureImport = () => {
@@ -20,7 +20,7 @@ export const PictureImport = () => {
       Alert("Choisir un fichier d'abord !");
     }
 
-    const storageRef = ref(storage, `/files/${file.name}`);
+    const storageRef = ref(storage, `/defaults/${file.name}`);
 
     // la progression peut être interrompue et reprise. Il expose également la progression des mises à jour.
     // Reçoit la référence de stockage et le fichier à uploader.
@@ -47,14 +47,20 @@ export const PictureImport = () => {
 
   return (
     <div className="pictureImport">
-      <input
-        type="file"
-        id="imageFile"
-        accept="image/*"
-        onChange={handleChange}
-      />
-      <button onClick={handleUpload}>Télécharger l'image</button>
-      <h3>{percentages}"% effectués"</h3>
+      <div className="pictureInput">
+        <input
+          type="file"
+          id="imageFile"
+          accept="image/*"
+          onChange={handleChange}
+        />
+      </div>
+      <button className="Pictures" onClick={handleUpload}>
+        Télécharger
+      </button>
+      <div className="Percentages">
+        <h3>{percentages} % effectués</h3>
+      </div>
     </div>
   );
 };
