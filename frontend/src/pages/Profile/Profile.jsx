c4b94508f15a197ed77d84c13982b75da85f04c0
antoine carpentier <a.carpentier.wcs@gmail.com>
2022-09-24 19:07:13 +0200

    css page profile

--- frontend/src/pages/Profile/Profile.jsx
+++ frontend/src/pages/Profile/Profile.jsx
@@ -3,6 +3,7 @@ import "./Profile.css";
 import { InputPassword } from "../../components/Input/InputPassword";
 import { Footer, Header, Input, Button } from "../../components/index";
 import { postProfile } from "../../services/axios/AxiosUsers";
+import logo from "../../assets/logo-essai1.png";
 
 const Profile = () => {
   const [cp, setCp] = useState("");
@@ -27,65 +28,84 @@ const Profile = () => {
 
         <form className="profile_champ-container">
           <h1>PROFIL</h1>
-          <Input
-            className="inputProfil"
-            forId="cp"
-            type="text"
-            champ="Numéro de CP"
-            onChange={(e) => setCp(e.target.value)}
-            value={cp}
-            // minlength="8"
-            // maxlength="8"
-          />
-          <Input
-            className="inputProfil"
-            forId="mail"
-            type="email"
-            champ="Adresse mail"
-            onChange={(e) => setMail(e.target.value)}
-            value={mail}
-          />
-          <Input
-            className="inputProfil"
-            forId="telephone"
-            type="tel"
-            champ="Téléphone"
-            onChange={(e) => setPhoneNumber(e.target.value)}
-            value={phone_number}
-          />
-          <InputPassword
-            className="inputProfil"
-            forId="mot"
-            champ="Mot de passe"
-            onChange={(e) => setPassword(e.target.value)}
-            autoComplete="on"
-            value={password}
-          />
-          <InputPassword
-            className="inputProfil"
-            forId="confirmation"
-            champ="Confirmation du mot de passe"
-            onChange={(e) => setSecondPassword(e.target.value)}
-            autoComplete="on"
-            value={secondPassword}
-          />
-          <Button
-            classButton="envoyer"
-            disabled={verifPasswords}
-            onClick={(e) =>
-              postProfile(
-                data,
-                setCp,
-                setMail,
-                setPhoneNumber,
-                setPassword,
-                setSecondPassword,
-                e
-              )
-            }
-            champButton="ENVOYER"
-            type="submit"
-          />
+          <div className="inputProfil1">
+            <Input
+              className="inputProfil"
+              forId="cp"
+              type="text"
+              champ="Numéro de CP"
+              onChange={(e) => setCp(e.target.value)}
+              value={cp}
+              // minlength="8"
+              // maxlength="8"
+            />
+          </div>
+
+          <div className="inputProfil2">
+            <Input
+              className="inputProfil"
+              forId="mail"
+              type="email"
+              champ="Adresse mail"
+              onChange={(e) => setMail(e.target.value)}
+              value={mail}
+            />
+          </div>
+
+          <div className="inputProfil3">
+            <Input
+              className="inputProfil"
+              forId="telephone"
+              type="tel"
+              champ="Téléphone"
+              onChange={(e) => setPhoneNumber(e.target.value)}
+              value={phone_number}
+            />
+          </div>
+
+          <div className="profile-logo">
+            <img className="logoProfile" src={logo} alt="Logo" />
+          </div>
+
+          <div className="inputProfil4">
+            <InputPassword
+              className="inputProfil"
+              forId="mot"
+              champ="Mot de passe"
+              onChange={(e) => setPassword(e.target.value)}
+              autoComplete="on"
+              value={password}
+            />
+          </div>
+          <div className="inputProfil5">
+            <InputPassword
+              className="inputProfil"
+              forId="confirmation"
+              champ="Confirmation du mot de passe"
+              onChange={(e) => setSecondPassword(e.target.value)}
+              autoComplete="on"
+              value={secondPassword}
+            />
+          </div>
+          <div className="envoyer">
+            <Button
+              classButton="envoyer"
+              disabled={verifPasswords}
+              onClick={(e) =>
+                postProfile(
+                  data,
+                  setCp,
+                  setMail,
+                  setPhoneNumber,
+                  setPassword,
+                  setSecondPassword,
+                  e
+                )
+              }
+              champButton="ENVOYER"
+              type="submit"
+            />
+          </div>
         </form>
 
         <Footer />
