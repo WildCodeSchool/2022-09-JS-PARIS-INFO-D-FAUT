58290d4f5f8d2614129e5807e65cb35fa9d99e92
Marina Alves <ad.marina@hotmail.fr>
2022-10-10 10:01:51 +0200

    modification authadminguard

--- frontend/src/services/AuthAdminGuar/AuthAdminGuard.jsx
+++ frontend/src/services/AuthAdminGuar/AuthAdminGuard.jsx
@@ -2,12 +2,45 @@ import React, { useContext } from "react";
 import { Navigate } from "react-router-dom";
 import { UserContext } from "../../context/index";
 
+// const test = () => {
+//   const { user } = useContext(UserContext);
+//   const cp = user.cp;
+//   console.log(cp)
+// }
+// test();
+
 const AuthAdminGuard = ({ children }) => {
   const { user } = useContext(UserContext);
   const admin = user.admin;
+  const cp = user.cp;
+
+  // const logout = async () => {
+  //   // e.preventDefault();
+  //   const token = localStorage.getItem("token");
+
+  //   const config = { headers: { Authorization: `Bearer ${token}` } };
+
+  //   const bodyParameters = {
+  //     key: "value",
+  //   };
+
+  //   const response = await axios.post(
+  //     `http://localhost:5000/logout`,
+  //     bodyParameters,
+  //     config
+  //   );
+  //   // nav();
+  //   localStorage.removeItem("token");
+  // };
+
+  // if (user === undefined){
+  //   return <Navigate to={`/home/${cp}`} />;
+  // }
 
   if (admin !== 1) {
-    return <Navigate to="/items" />;
+    // console.log("cp", cp)
+    // logout();
+    return <Navigate to="/" />;
   }
   return children;
 };
